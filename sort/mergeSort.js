b11de645124814b40603f62aed3d8e943a501be0
wilson <wilson@gdn-commerce.com>
2021-11-19 17:41:56 +0700

    fix merge sort

--- sort/mergeSort.js
+++ sort/mergeSort.js
@@ -26,6 +26,12 @@ const merge = (arr, l, m, r) => {
         k++
     }
 
+    while (i < n1) {
+        arr[k] = L[i]
+        i++
+        k++
+    }
+
     while (j < n2) {
         arr[k] = R[j]
         j++
